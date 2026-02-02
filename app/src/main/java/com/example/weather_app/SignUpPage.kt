f7669cf97e92781c20998a93ed4f8b63a7c36125
Peter <petergloag@gmail.com>
2026-02-02 13:56:47 -0800

    Added SignInPage activity and view. Created the groundwork to add functionality. Next Step: combine with SignInPage and DB.

---
+++ app/src/main/java/com/example/weather_app/SignUpPage.kt
@@ -0,0 +1,40 @@
+package com.example.weather_app
+
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SignUpPage : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_sign_up_page)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    public fun SignUp(view : View?) {
+        Toast.makeText(this, "SINGING YOU IN...", Toast.LENGTH_SHORT).show()    // Debug just to show that button works
+        // Get values of username and password field
+        var username = findViewById<EditText>(R.id.userNameEditText).text
+        var password = findViewById<EditText>(R.id.passwordEditText).text
+        if(username.isEmpty() || password.isEmpty()){
+            Toast.makeText(this, "Username and password files must not be blank", Toast.LENGTH_SHORT).show()
+            return
+        }
+        // Add user to DB
+    }
+    public fun SignIn(view : View?) {
+        Toast.makeText(this, "REDIRECTING TO SIGN IN PAGE...", Toast.LENGTH_SHORT).show()
+        // Start SignInPage activity
+    }
+}
