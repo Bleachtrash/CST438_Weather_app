83e362395142ef0fd06ab3e1a6f24015f7a67308
Peter <petergloag@gmail.com>
2026-02-12 16:16:09 -0800

    Added dependencies for NoaaApi to build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -89,4 +89,12 @@ dependencies {
     implementation(libs.androidx.room.runtime.v284)
     implementation("com.google.android.gms:play-services-location:21.3.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
+
+    implementation("com.squareup.moshi:moshi-kotlin:1.15.2")
+    ksp("com.squareup.moshi:moshi-kotlin-codegen:1.15.2")
+    implementation("com.squareup.okhttp3:okhttp:5.3.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:5.3.0")
+    implementation("com.squareup.retrofit2:retrofit:3.0.0")
+    implementation("com.squareup.retrofit2:converter-moshi:3.0.0")
+
 }
